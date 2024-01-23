fe7fd0874820babc37f578390a4a29f5296505c5
cher-yeros <yerosendiriba1892@gmail.com>
2024-01-23 23:50:19 +0300

    hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import JsWrapper from "@/components/JsWrapper";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+import "../assets/css/bootstrap.min.css";
 import "../assets/css/style.css";
 import "../assets/css/tiny-slider.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children} <JsWrapper />
+        {children}
+        {/* <JsWrapper /> */}
+        <Script src="/assets/js/bootstrap.bundle.min.js" />
+        <Script src="/assets/js/custom.js" />
+        <Script src="/assets/js/tiny-slider.js" />
       </body>
     </html>
   );
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -8,9 +8,8 @@ export default function Navbar() {
     >
       <div className="container">
         <a className="navbar-brand" href="index.html">
-          Furni<span>.</span>
+          Grace Furniture<span>.</span>
         </a>
-
         <button
           className="navbar-toggler"
           type="button"
@@ -23,7 +22,7 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarsFurni">
+        <div className="navbar-collapse" id="navbarsFurni">
           <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
             <li className="nav-item active">
               <a className="nav-link" href="index.html">
@@ -56,7 +55,6 @@ export default function Navbar() {
               </a>
             </li>
           </ul>
-
           <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
             <li>
               <a className="nav-link" href="#">
